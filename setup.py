3dee17e48c90e05bc3a318c7f847c7311c470e7f
yanielajanis <yanielajanis@gmail.com>
2022-02-28 11:29:34 -0300

    actualizando bug matplotlib

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
   install_requires=[            # I get to this in a second
           'pandas',
           'seaborn',
-          'matplotlib.pyplot',
+          'matplotlib',
           'datetime',
           'numpy',
           'july',
